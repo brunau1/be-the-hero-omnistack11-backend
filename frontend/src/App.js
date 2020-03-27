04e7910b8a55fd2917801308fc5c92be95c67667
brunau1 <brunosantosprotec@gmail.com>
2020-03-27 00:28:02 -0300

    frontend react created starting integration to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React from "react";
+
+import Routes from "./routes";
+import "./global.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  function increment() {
-    setCounter(counter + 1);
-  }
-  return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
